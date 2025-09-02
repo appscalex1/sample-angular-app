e9171b7e2d427fb5fc8a756dd0ad5c938580d232
appscalex1 <158258344+appscalex1@users.noreply.github.com>
2025-09-02 16:49:37 +0530

    Create environment.ts

---
+++ environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  apiKey: 'AKIAEXAMPLESECRETKEY123456',  // <<< This is a "leak"
+  dbPassword: 'MySuperSecretPassword'    // <<< Another "leak"
+};
