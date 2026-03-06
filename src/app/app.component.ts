b3a8923c3b9da0886037e896f5d193c00ce6f1dc
appscalex1 <158258344+appscalex1@users.noreply.github.com>
2026-03-06 22:27:53 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>{{title}}</h1>
+    <button (click)="increment()">+</button>
+    <button (click)="decrement()">-</button>
+  `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'sample-angular-app';
+  counter = 0;
+  name = '';
+
+  ngOnInit() {}
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+
 }
